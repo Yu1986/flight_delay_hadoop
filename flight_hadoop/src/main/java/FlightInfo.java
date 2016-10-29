7a1d9d72fc9f6ebb8e28d8b67907b2274ba597dc
jy <jy@jy.com>
2016-10-29 00:45:40 -0700

    add combinner for flight info

--- flight_hadoop/src/main/java/FlightInfo.java
+++ flight_hadoop/src/main/java/FlightInfo.java
@@ -1,23 +1,51 @@
-import org.apache.commons.configuration.SystemConfiguration;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 
 public class FlightInfo {
+
+    private static class Delay implements Writable {
+        public long sum;
+        public int cnt;
+        public Delay(long sum, int cnt) {
+            this.sum = sum;
+            this.cnt = cnt;
+        }
+
+        public Delay() {
+            this.sum = 0;
+            this.cnt = 0;
+        }
+
+        public void write(DataOutput dataOutput) throws IOException {
+            dataOutput.writeLong(sum);
+            dataOutput.writeInt(cnt);
+        }
+
+        public void readFields(DataInput dataInput) throws IOException {
+            sum = dataInput.readLong();
+            cnt = dataInput.readInt();
+        }
+    }
+
     public static class FlightInfoMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
+            extends Mapper<Object, Text, Text, Delay> {
 
         private Text route = new Text();
-        private IntWritable route_delay = new IntWritable();
+        private Delay route_delay = new Delay(0, 0);
+        private long cnt = 0;
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
@@ -40,14 +68,14 @@ public class FlightInfo {
 
                 int[] cols_index = {14, 16, 17};
                 String[] cols = getCol(line.toCharArray(), ',', cols_index);
-                int delay_num = 0;
+                long delay_num = 0;
                 if (!cols[0].equals("NA")) {
                     delay_num = Integer.parseInt(cols[0]);
                 }
                 route.set(String.format("%s-%s", cols[1], cols[2]));
-                route_delay.set(delay_num);
+                route_delay.sum = delay_num;
+                route_delay.cnt = 1;
                 context.write(route, route_delay);
-
             }
         }
 
@@ -80,18 +108,37 @@ public class FlightInfo {
         }
     }
 
-    public static class FlightInfoReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
+    public static class FlightInfoCombiner
+            extends Reducer<Text, Delay, Text, Delay> {
+        private Delay route_delay = new Delay(0, 0);
 
-        public void reduce(Text key, Iterable<IntWritable> values,
+        public void reduce(Text key, Iterable<Delay> values,
                            Context context
         ) throws IOException, InterruptedException {
             long sum = 0; // prevent sum overflow.
             int cnt = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-                cnt++;
+            for (Delay val : values) {
+                sum += val.sum;
+                cnt += val.cnt;
+            }
+            route_delay.sum = sum;
+            route_delay.cnt = cnt;
+            context.write(key, route_delay);
+        }
+    }
+
+    public static class FlightInfoReducer
+            extends Reducer<Text, Delay, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<Delay> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            long sum = 0; // prevent sum overflow.
+            int cnt = 0;
+            for (Delay val : values) {
+                sum += val.sum;
+                cnt += val.cnt;
             }
             int avg = (int)(sum/cnt);
             result.set(avg);
@@ -105,12 +152,13 @@ public class FlightInfo {
         job.setJarByClass(Flight.class);
 
         job.setMapperClass(FlightInfoMapper.class);
+        job.setCombinerClass(FlightInfoCombiner.class);
         job.setReducerClass(FlightInfoReducer.class);
         job.setNumReduceTasks(1);
-
         job.setOutputKeyClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Delay.class);
         job.setOutputValueClass(IntWritable.class);
-
         FileInputFormat.addInputPath(job, new Path(args[0]));
 
         Path outPath = new Path(args[1]);
@@ -118,5 +166,6 @@ public class FlightInfo {
         FileOutputFormat.setOutputPath(job, outPath);
 
         job.waitForCompletion(true);
+
     }
 }
